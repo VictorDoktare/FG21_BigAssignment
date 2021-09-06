046a3fbd2210dfa531a5dd480f49f509b1d445d1
Victor Doktare <victor@doktare.com>
2021-09-06 10:42:36 +0200

    Directional Movement DONE

--- Assets/Scripts/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Scripts/Player/PlayerController.cs
@@ -10,24 +10,53 @@ namespace FG
 		
 		private bool isMoving;
 
+		#region Unity Event Functions
+
 		private void Update()
 		{
-			if (isMoving) return;
+			InputDirection();
+		}
 
+		#endregion
+		
+		//Directional Input
+		void InputDirection()
+		{
+			//Makes sure movement is complete before checking for new input
+			if (isMoving) return;
+			
+			//Input Direction X-Axis
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				RollDirection(-0.5f, Vector3.left);
+			}
 			if (Input.GetKeyDown(KeyCode.RightArrow))
 			{
-				var pivotAnchor = transform.position + new Vector3(-0.5f, -0.5f, 0f);
-				var pivotAxis = Vector3.Cross(Vector3.up, Vector3.left);
-				StartCoroutine(RollPlayer(pivotAnchor, pivotAxis));
+				RollDirection(0.5f, Vector3.right);
+			}
+			
+			//Input Direction Z-Axis
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				RollDirection(0.5f, Vector3.forward);
+			}
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				RollDirection(-0.5f, Vector3.back);
 			}
 		}
 
-		void RollDirection(float direction)
+		//Set the pivot and axis which the cube is rotating from
+		void RollDirection(float direction, Vector3 vector)
 		{
+			var pivotAnchor = transform.position + new Vector3(direction, -0.5f, direction);
+			var pivotAxis = Vector3.Cross(Vector3.up, vector);
 			
+			StartCoroutine(RollCube(pivotAnchor, pivotAxis));
 		}
 
-		IEnumerator RollPlayer(Vector3 anchor, Vector3 axis)
+		//Rotate cube and correct eventual offsets
+		IEnumerator RollCube(Vector3 anchor, Vector3 axis)
 		{
 			isMoving = true;
 
@@ -43,7 +72,6 @@ namespace FG
 			transform.eulerAngles = new Vector3(Mathf.Round(transform.eulerAngles.x), Mathf.Round(transform.eulerAngles.y), Mathf.Round(transform.eulerAngles.z));
 
 			isMoving = false;
-
 		}
 
 	}
