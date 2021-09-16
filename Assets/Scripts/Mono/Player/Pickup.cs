bf523da70c2b8c8628e698eed91673776bc0f463
Victor Doktare <victor@doktare.com>
2021-09-16 16:52:29 +0200

    Pickup VFX done

--- Assets/Scripts/Mono/Player/Pickup.cs
+++ Assets/Scripts/Mono/Player/Pickup.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,13 +7,21 @@ namespace FG
 {
 	public class Pickup : MonoBehaviour
 	{
-		public static event Action OnPlayerPickup;
+		//public static event Action OnPlayerPickup;
+		[SerializeField] private GameObject pickupVFX;
+
+		private void Update()
+		{
+			transform.Rotate(0, 45 * Time.deltaTime, 0);
+		}
+
 		private void OnTriggerEnter(Collider other)
 		{
 			if (other.tag == "Player")
 			{
 				GameManager.Instance.AddPickup();
 				GameManager.Instance.CheckForLevelClear();
+				Instantiate(pickupVFX, transform.position, quaternion.identity);
 				Destroy(gameObject);
 			}
 		}
